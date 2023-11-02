636ce8b4d683a7fa538746598d941e3f5441e0ca
Tao Wang <tw520@ic.ac.uk>
2023-11-02 12:26:22 +0000

    Feat: Begin creating single module of excitatory-excitatory population

--- q1.py
+++ q1.py
@@ -7,3 +7,14 @@ from iznetwork import IzNetwork
 # 3. Create 200x200 inhibitory population and connect to excitatory populations (1000x1000 weight matrix)
 # 5. Rewiring process
 # 6. Connectivitiy matrix, raster plot and mean firing rate over each probability p.
+
+# 1. Create 100x100 excitatory module, with weights and delays 
+
+N = 100
+Dmax = 20 
+ExNet = IzNetwork(N, Dmax)
+
+W = np.zeros((N, N))
+D = np.zeros((N, N))
+
+
