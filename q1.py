419718bdfb4d844d993574810464c1f0c3b9f864
Tao Wang <tw520@ic.ac.uk>
2023-11-03 11:12:17 +0000

    Feat: Implement weight and delay matrix for entire network and begin testing

--- q1.py
+++ q1.py
@@ -13,31 +13,14 @@ from iznetwork import IzNetwork
 # Implementations
 # 1. Create 100x100 excitatory module, with weights and delays 
 
-N = 100     # Number of neurons in excitatory-excitatory module
-F = 17      # Scaling factor for excitatory-excitatory connections    
-Dmax = 20   # Maximum conduction delay 
-ExNet = IzNetwork(N, Dmax)
 
-W = np.zeros((N, N))
-D = np.zeros((N, N))
+N_ex = 100     # Number of neurons in excitatory module
+N_in = 200     # Number of neurons in inhibitory module 
+N_mod = 8      # Number of modules in the modular network 
+N_net = 1000   # Number of neurons in the modular network 
 
-all_idxs = []
-for i in range(N):
-    for j in range(N):
-        if i == j:
-            continue
-        all_idxs.append((i, j))
-# idx = np.array(random.sample(all_idxs, 1000))
-idxs = random.sample(all_idxs, 1000)
-
-# Set connection weight to be 1 for randomly generated connections
-# Set random delays for these connections simultaneously. 
-for idx in idxs:
-    W[idx] = 1*F 
-    D[idx] = np.random.randint(1, Dmax+1) # Assume delays have to be integer values in range [1,20], need to double check 
-
-# Sanity check - make sure indexes are the same in both matrices
-print(np.where(W != 0)[0] == np.where(D != 0)[0])
+F_ex_in = 50
+F_in_ex = 2
 
 # 2. Extend to 8 populations
 
@@ -50,8 +33,7 @@ def ExcitatoryToExcitatory(N):
     W - The connectivity matrix for the excitatory-to-excitatory community. 
     D - The conduction delay matrix for the excitatory-to-excitatory community. 
     '''
-
-    N = 100     # Number of neurons in excitatory-excitatory module
+    
     F = 17      # Scaling factor for excitatory-excitatory connections    
     Dmax = 20   # Maximum conduction delay 
     
@@ -64,7 +46,7 @@ def ExcitatoryToExcitatory(N):
             if i == j:
                 continue
             all_idxs.append((i, j))
-    # idx = np.array(random.sample(all_idxs, 1000))
+
     idxs = random.sample(all_idxs, 1000)
 
     # Set connection weight to be 1 for randomly generated connections
@@ -76,40 +58,38 @@ def ExcitatoryToExcitatory(N):
     return W, D
 
 # 3. Create 200x200 inhibitory population and connect to excitatory populations (1000x1000 weight matrix)
-
-# Option 1: One giant block construction
-exWeights, delays = [ExcitatoryToExcitatory(N) for _ in range(8)]
-
-oneBlock  = np.ones((N, N))
-zeroBlock = np.zeros((N, N))
-
-# Block [i,j] is the connection from population i to population j
-W = np.bmat([[ex1,       zeroBlock,  zeroBlock,  zeroBlock, zeroBlock, zeroBlock, zeroBlock, zeroBlock, ex-to-inhib],
-             [zeroBlock, ex2,        zeroBlock,  zeroBlock, zeroBlock, zeroBlock, zeroBlock, zeroBlock, ex-to-inhib],
-             [zeroBlock, zeroBlock,  ex3,        zeroBlock, zeroBlock, zeroBlock, zeroBlock, zeroBlock, ex-to-inhib],
-             [zeroBlock, zeroBlock,  zeroBlock,  ex4,       zeroBlock, zeroBlock, zeroBlock, zeroBlock, ex-to-inhib],
-             [zeroBlock, zeroBlock,  zeroBlock,  zeroBlock, ex5,       zeroBlock, zeroBlock, zeroBlock, ex-to-inhib],
-             [zeroBlock, zeroBlock,  zeroBlock,  zeroBlock, zeroBlock, ex6,       zeroBlock, zeroBlock, ex-to-inhib],
-             [zeroBlock, zeroBlock,  zeroBlock,  zeroBlock, zeroBlock, zeroBlock, ex7,       zeroBlock, ex-to-inhib],
-             [zeroBlock, zeroBlock,  zeroBlock,  zeroBlock, zeroBlock, zeroBlock, zeroBlock, ex8      , ex-to-inhib],
-             [inhib-to-all-excitatory,                                                                  inhib-to-all-inhib]])
-
-# Option 2 - I think this is more intuitive:  
+  
 W = np.zeros([1000, 1000])
-D = np.zeros([1000, 1000])
-# Then use numpy slice notation to modify certain blocks.
+D = np.ones([1000, 1000])
 
 # Update excitatory-excitatory blocks
-for i in range(0, 800, 100):
+for i in range(0, 800, N_ex):
     Wblock, Dblock = ExcitatoryToExcitatory(N)
-    W[i:i+N, i:i+N] = Wblock
-    D[i:i+N, i:i+N] = Dblock 
+    W[i:i+N_ex, i:i+N_ex] = Wblock
+    D[i:i+N_ex, i:i+N_ex] = Dblock 
 
 # Update excitatory-to-inhibitory block
-# Reminder to self: check explanation on my notes
+for j in range(N_mod*N_ex, N_net):
+    i = np.random.randint(0, N_mod) 
+    neurons = random.sample(range(N_ex), 4)
+    i = neurons + N_ex*i
+    W[i,j] = 50*random.uniform(0, 1)
 
-# Update inhibitory-to-excitatory block 
-W[800:,:800] = 
-D[800:,:800] = np.ones((200, 8*N))
+# Update inhibitory
+in_to_ex_block = F_in_ex * np.random.uniform(0, 1, (N_in, N_mod*N_ex)) 
+in_to_in_block = np.random.uniform(0, 1, (N_in, N_in))
+W[800:, :] = np.column_stack((in_to_ex_block, in_to_in_block))
 
-# Update inhibitory-inhibitory block 
+# Inhibitory neurons cannot connect to themselves, thus set the connection weight to 0. 
+W[range(800, 1000),range(800, 1000)] = 0 
+
+net = IzNetwork(N_net, 20)
+a = 0.02 * np.ones([N_net])
+b = np.column_stack((0.2*np.ones([800]), 0.25*np.ones([200])))
+c = -65 * np.ones([N_net])
+d = np.column_stack((8*np.ones([800]), 2*np.ones([200])))
+net.setParameters(a, b, c, d)
+net.setWeights(W)
+net.setDelays(D)
+# 5. Rewiring process
+# 6. Connectivitiy matrix, raster plot and mean firing rate over each probability p.
