c62968c6ca67828d6c9ae099ec0e8b046d196e66
Tao Wang <tw520@ic.ac.uk>
2023-11-03 12:06:52 +0000

    Feat: p=0 WTA mechanism working

--- q1.py
+++ q1.py
@@ -38,7 +38,7 @@ def ExcitatoryToExcitatory(N):
     Dmax = 20   # Maximum conduction delay 
     
     W = np.zeros((N,N))
-    D = np.zeros((N,N))
+    D = np.ones((N,N), dtype=int)
 
     all_idxs = []
     for i in range(N):
@@ -60,36 +60,52 @@ def ExcitatoryToExcitatory(N):
 # 3. Create 200x200 inhibitory population and connect to excitatory populations (1000x1000 weight matrix)
   
 W = np.zeros([1000, 1000])
-D = np.ones([1000, 1000])
+D = np.ones([1000, 1000], dtype=int)
 
 # Update excitatory-excitatory blocks
 for i in range(0, 800, N_ex):
-    Wblock, Dblock = ExcitatoryToExcitatory(N)
+    Wblock, Dblock = ExcitatoryToExcitatory(N_ex)
     W[i:i+N_ex, i:i+N_ex] = Wblock
     D[i:i+N_ex, i:i+N_ex] = Dblock 
 
 # Update excitatory-to-inhibitory block
 for j in range(N_mod*N_ex, N_net):
     i = np.random.randint(0, N_mod) 
-    neurons = random.sample(range(N_ex), 4)
+    neurons = np.array(random.sample(range(N_ex), 4))
     i = neurons + N_ex*i
     W[i,j] = 50*random.uniform(0, 1)
 
 # Update inhibitory
-in_to_ex_block = F_in_ex * np.random.uniform(0, 1, (N_in, N_mod*N_ex)) 
-in_to_in_block = np.random.uniform(0, 1, (N_in, N_in))
+in_to_ex_block = F_in_ex * np.random.uniform(-1, 0, (N_in, N_mod*N_ex)) 
+in_to_in_block = np.random.uniform(-1, 0, (N_in, N_in))
 W[800:, :] = np.column_stack((in_to_ex_block, in_to_in_block))
 
 # Inhibitory neurons cannot connect to themselves, thus set the connection weight to 0. 
 W[range(800, 1000),range(800, 1000)] = 0 
 
 net = IzNetwork(N_net, 20)
-a = 0.02 * np.ones([N_net])
-b = np.column_stack((0.2*np.ones([800]), 0.25*np.ones([200])))
-c = -65 * np.ones([N_net])
-d = np.column_stack((8*np.ones([800]), 2*np.ones([200])))
+a = 0.02 * np.ones(N_net)
+b = np.concatenate((0.2*np.ones(800), 0.25*np.ones(200)))
+c = -65 * np.ones(N_net)
+d = np.concatenate((8*np.ones(800), 2*np.ones(200)))
 net.setParameters(a, b, c, d)
 net.setWeights(W)
 net.setDelays(D)
+
+T = 1000
+V = np.zeros((T, N_net))
+for t in range(T):
+    I = 15*np.random.poisson(0.01, N_net) 
+    net.setCurrent(I)
+    net.update()
+    V[t,:], _ = net.getState()
+
+t, n = np.where(V > 29)
+plt.scatter(t, n)
+plt.ylim(800, 0)
+plt.xlabel('Time (ms)')
+plt.ylabel('Neuron index')
+plt.show()
+
 # 5. Rewiring process
 # 6. Connectivitiy matrix, raster plot and mean firing rate over each probability p.
